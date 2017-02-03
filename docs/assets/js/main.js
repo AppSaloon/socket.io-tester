33de592fa7803fd4beeb2b13826e0da48bfaa094
Mi3k3 <mieke.nijs@gmail.com>
2017-02-03 11:27:38 +0100

    change image depending on browser

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
-  console.log('test');
-  console.log('browser: ', $.browser);
+  var img_src = "http://placehold.it/800x600"
+  if (navigator.userAgent.search("MSIE") >= 0) {
+    img_src = "http://lorempixel.com/output/nightlife-q-g-800-600-6.jpg"
+  }
+  else if (navigator.userAgent.search("Chrome") >= 0) {
+      img_src = "http://lorempixel.com/output/food-q-g-800-600-7.jpg"
+  }
+  else if (navigator.userAgent.search("Firefox") >= 0) {
+      img_src = "http://lorempixel.com/output/sports-q-g-800-600-10.jpg"
+  }
+  else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+      img_src = "http://lorempixel.com/output/technics-q-g-800-600-4.jpg"
+  }
+  else if (navigator.userAgent.search("Opera") >= 0) {
+      img_src = "http://lorempixel.com/output/nightlife-q-g-800-600-5.jpg"
+  }
+  $("#screenshot").attr("src", img_src);
 });
