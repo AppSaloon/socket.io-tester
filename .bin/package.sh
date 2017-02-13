fd133b71bb1330d5ecdb7ef362bc2e6433ec4385
Jo Jordens <jo@appsaloon.be>
2017-02-13 14:58:47 +0100

    removed electron version parameter

--- .bin/package.sh
+++ .bin/package.sh
@@ -1,3 +1,4 @@
 export NODE_ENV=production
 echo "starting build"
-node_modules/.bin/electron-packager app --platform=darwin,linux,win32 --arch=all --asar --icon ./icon.jpg --out packages --electron-version v1.6.0
+node_modules/.bin/electron-packager app --platform=darwin,linux,win32 --arch=all --asar --icon ./icon.jpg --out packages
+# node_modules/.bin/electron-packager app --platform=darwin,linux,win32 --arch=all --asar --icon ./icon.jpg --out packages --electron-version 1.6.0
